1ea9f918173c21aba372b1f19bec2cef128343b3
root <juanlds621@gmail.com>
2022-06-15 18:44:54 -0700

    assembler

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
-* printf message in C
-*/
+ * main - prints the phrase
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
