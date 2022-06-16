bbe51fd3ecd5302936e26d98cb0d7953636a7955
root <juanlds621@gmail.com>
2022-06-15 18:58:03 -0700

    printf message

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main: print the phrase with proper grammar, but the outcome is a piece of art,
+ * main: print the phrase with proper grammar
+ * but the outcome is a piece of art,
  * return: 0 if exited properly
  */
 int main(void)
