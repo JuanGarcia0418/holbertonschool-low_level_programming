0d7160dff1b67c3be4a2a7490fa906fd5d79aefb
root <juanlds621@gmail.com>
2022-06-17 22:34:45 -0700

    alphabet with putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-  char a;
-  for (a = 'a'; a <= 'z'; a++)
-    {
-      putchar(a);
-    }
-  putchar('\n');
-    return (0);
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
