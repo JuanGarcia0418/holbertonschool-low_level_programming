ce8b0c79b029ebdbf8ef6b76d3f33e6749119801
root <juanlds621@gmail.com>
2022-06-19 16:55:54 -0700

    lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - check lowercase pharase
+ * @c : variable of aphabet
+ * Return: 0 and 1
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
