ad5b122953a83834da21b58db80110e426899cf4
root <juanlds621@gmail.com>
2022-06-19 14:08:30 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - make the alphabet
+ */
+void print_alphabet(void)
+{
+char alp;
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+_putchar(alp);
+}
+_putchar('\n');
+}
