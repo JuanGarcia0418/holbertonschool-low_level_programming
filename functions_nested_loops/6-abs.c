7324adbe76b99b51886b3eb0d62403c1c0a26e9f
root <juanlds621@gmail.com>
2022-06-20 10:38:31 -0700

    values

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - outputs absolut value of integer input
+ * @n : integer to check
+ * Return: 0 or -1
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+return (0);
+}
