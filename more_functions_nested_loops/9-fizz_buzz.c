b64cab47030a1a904062a4dbf4d4f4ee5297afa4
root <juanlds621@gmail.com>
2022-06-21 17:22:08 -0700

    task of fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - start of code
+ * Return: number if true
+ */
+int main(void)
+{
+int nums;
+for (nums = 1; nums <= 100; nums++)
+{
+if (nums % 15 == 0)
+printf("Fizzbuzz");
+else if (nums % 3 == 0)
+printf("Fizz");
+else if (nums % 5 == 0)
+printf("Buzz");
+else
+printf("%i", nums);
+if (nums < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
