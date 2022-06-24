29c16fb68e113da1ddbf00741ea5e2d38ea77e6e
root <juanlds621@gmail.com>
2022-06-24 14:55:45 -0700

    print part of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - print part of string
+ * @str : variable with numbers
+ */
+void puts_half(char *str)
+{
+int pri, cou = 0;	
+while (*(str + cou))
+cou++;
+pri = cou / 2;
+if (pri % 2)
+pri += 1;
+while (pri < cou)
+{
+_putchar(*(str + pri));
+pri++;
+}
+ _putchar('\n');
+}
