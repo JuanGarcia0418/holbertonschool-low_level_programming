061892438c32cae041e6548a7cfb0e4d3a549b6b
root <juanlds621@gmail.com>
2022-06-24 08:16:38 -0700

    reverse string to other string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverse string in other string
+ * @s : string to reverse
+ */
+void rev_string(char *s)
+{
+int i = 0, j = 0;
+char str[10];
+while (*(s + i))
+{
+*(str + 1) = *(s + i);
+i++;
+}
+i = i - 1;
+while (i >= 0)
+{
+*(s + i) = *(str + j);
+j++;
+i--;
+}
+}
