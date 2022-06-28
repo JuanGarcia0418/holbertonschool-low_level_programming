5b615275ef534eb25b0d7a00288067b5529f29a8
root <juanlds621@gmail.com>
2022-06-28 15:28:15 -0700

    concatenate 2 strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcat - content of string
+ * @dest : first part of a string
+ * @src : second part of a string
+ * Return: first part of string
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j = 0;
+while (*(dest + i))
+i++;
+while ((*(dest + i) = *(src + j)))
+{
+i++;
+j++;
+}
+return (dest);
+}
