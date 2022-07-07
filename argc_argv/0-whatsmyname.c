75769e92dd0ba25cdc486849e558a3376b0965be
root <juanlds621@gmail.com>
2022-07-06 20:49:59 -0700

    print compile program

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to string
+ * Return: if true 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
