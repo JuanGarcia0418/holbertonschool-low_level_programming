7449a2265a15331b5c47467ba82af73ad5fe7e21
root <juanlds621@gmail.com>
2022-07-06 20:59:07 -0700

    print number of content

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to string
+ * Return: if true 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+		return (0);
+}
