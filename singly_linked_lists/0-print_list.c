17c365169c01928ae849ea9e2096142aee61be4e
Juan Garcia <juangarcia@JuanGarcia.local>
2022-07-26 01:30:04 -0500

    add task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include "stdio.h"
+/**
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *tmp;
+	unsigned int i;
+
+	tmp = h;
+	for (i = 0; tmp; i++)
+	{
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+	}
+	return (i);
+}
