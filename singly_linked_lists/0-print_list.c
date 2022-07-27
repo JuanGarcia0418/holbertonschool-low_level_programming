444a8ab5f02d651c40c4203fcc6a8e923d637a69
Juan Garcia <juanlds621@gmail.com>
2022-07-26 23:36:34 -0500

    add doc betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,6 +4,7 @@
  * print_list - print all the elements of a list
  * @h: pointer to the list
  * Return: the number of nodes
+ *
  */
 size_t print_list(const list_t *h)
 {
@@ -11,9 +12,9 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-if (!h->str)
-printf("[0] (nil)\n");
-else
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
