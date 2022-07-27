3576cbb13d46a0e23ab1c37a0e72679cc45235a2
Juan Garcia <juanlds621@gmail.com>
2022-07-26 21:29:22 -0500

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,9 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
 /**
  * print_list - print all the elements of a list
  * @h: pointer to the list
  * Return: the number of nodes
- *
  */
 size_t print_list(const list_t *h)
 {
