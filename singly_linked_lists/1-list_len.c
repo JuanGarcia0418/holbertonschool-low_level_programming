847dcee87b17b3e03a7b1f48521ec521c2db9ff6
Juan Garcia <juanlds621@gmail.com>
2022-07-28 13:07:20 -0500

    print elements in a linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - finds the number of elements in a list
+ * @h: pointer of the linked list
+ * Return: number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *pl;
+	unsigned int i;
+
+	pl = h;
+	for (i = 0; pl; i++)
+		pl = pl->next;
+	return (i);
+}
