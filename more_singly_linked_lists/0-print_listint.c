67d0fcd4d2909ad954ad607a0660c77031c8cffd
Juan Garcia <juanlds621@gmail.com>
2022-07-28 21:46:15 -0500

    task 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -4,12 +4,11 @@
  * print_listint - print content of listint_t
  * @h: list to print
  * Return: number of elements
- *
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
