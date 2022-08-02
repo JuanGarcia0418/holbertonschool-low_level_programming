5dc0b2586a2b866b03f301a81b5f94c207cd08ed
Juan Garcia <juanlds621@gmail.com>
2022-08-02 12:05:36 -0500

    solving buff

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int ffrom, fto, rd, clf, clt;
-	char buff[BUFSIZ];
+	char buff[1295];
 
 	if (argc != 3)
 	{
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
+	while ((rd = read(ffrom, buff, 1295)) > 0)
 		if (fto == -1 || (write(fto, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
