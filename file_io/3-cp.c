b4a6db7e69e07c6c36f7bf1590fe47df76d91a9c
Juan Garcia <juanlds621@gmail.com>
2022-08-02 12:02:37 -0500

    solving error of characters

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,36 +10,41 @@
  */
 int main(int argc, char *argv[])
 {
-	int fr, fw, rlength, wlength, rclose, wclose;
-	char buf[1295];
-	mode_t m = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int ffrom, fto, rd, clf, clt;
+	char buff[BUFSIZ];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fr = open(argv[1], O_RDONLY);
-	if (fr == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	fw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, m);
-	if (fw == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	rlength = wlength = 1;
-	while (rlength)
 	{
-		rlength = read(fr, buf, 1295);
-		if (rlength == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-		if (rlength > 0)
-		{
-			wlength = write(fw, buf, rlength);
-			if (wlength == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);			
-		}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	rclose = close(fr);
-	if (rclose == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fr), exit(100);
-	wclose = close(fw);
-	if (wclose == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fw), exit(100);
+	ffrom = open(argv[1], O_RDONLY);
+	if (ffrom == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
+		if (fto == -1 || (write(fto, buff, rd) != rd))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	clf = close(ffrom);
+	clt = close(fto);
+	if (clf == -1 || clt == -1)
+	{
+		if (clf == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffrom);
+		else if (clt == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto);
+		exit(100);
+	}
 	return (0);
 }
